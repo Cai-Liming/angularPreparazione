3d7e2672dec0253135e156394009a6a7f3066356
Cai-Liming <cai.liming@iisgalvanimi.edu.it>
2022-09-26 09:54:37 +0000

    001.2

---
+++ tuo-cognome/src/app/terzo/terzo.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-terzo',
+  templateUrl: './terzo.component.html',
+  styleUrls: ['./terzo.component.css']
+})
+export class TerzoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  title = "quarto";
+
+}
